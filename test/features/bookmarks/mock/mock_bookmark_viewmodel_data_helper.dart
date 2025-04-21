ec333ba386409a0d8193d58af6e07f87667a4947
Sudipta-BS23 <anikc795@gmail.com>
2025-04-22 00:15:19 +0600

    added unit test for BookmarkViewmodel

---
+++ test/features/bookmarks/mock/mock_bookmark_viewmodel_data_helper.dart
@@ -0,0 +1,9 @@
+import 'package:kagi_news_app/features/news_list/data/models/news/news.dart';
+
+class MockBookmarkViewmodelDataHelper {
+  final bookmarks = [
+    News(title: "News1"),
+    News(title: "News2"),
+    News(title: "News3"),
+  ];
+}
